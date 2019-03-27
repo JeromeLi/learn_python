6bc01b2f89c08a4e17df6faf31e3fc10f4c7089a
Jerome <jerome.lzh@gmail.com>
2019-03-27 15:06:42 +0800

    fix number range detect

--- calc_fishc.py
+++ calc_fishc.py
@@ -1,6 +1,6 @@
-input_num=input('Please input a number from 1 to 100:')
-if int(input_num)==88:
+input_num = input('Please input a number from 1 to 100:')
+if 1 <= int(input_num) <= 100:
     print('You are a pretty girl!')
 else:
     print('You are an ugly man!')
-    
+
