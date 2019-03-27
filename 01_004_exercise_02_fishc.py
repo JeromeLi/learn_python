103a63dee15f3f5e9caf9f23d76b81b35d73473c
Jerome <jerome.lzh@gmail.com>
2019-03-27 20:55:13 +0800

    code optimization

--- 01_004_exercise_02_fishc.py
+++ 01_004_exercise_02_fishc.py
@@ -1,4 +1,5 @@
 temp_int = input('Please input a positive integer number:')
+print (isinstance(temp_int, int))
 loop_num = int(temp_int)
 while loop_num:
     print(' ' * (loop_num - 1) + '*' * loop_num)
--- The_first_game.py
+++ The_first_game.py
@@ -13,17 +13,21 @@ while i <= 3:
 		print('First time!')
 	else:
 		continue
-	temp = input('Guess a number:')
+	while True:
+		temp = input('Guess a number:')
+		if temp.isdigit():
+			break
+		else:
+			print('Input Error!')
 	guess = int(temp)
 	if guess == secret_num:
 		print('Bingo! You are right!')
 		print(':\)')
 		break
 	else:
-		if guess <= secret_num:
+		if guess < secret_num:
 			print('Too small!')
 		else:
 			print('Too large!')
 	i = i + 1
-
 print('Game over!')
