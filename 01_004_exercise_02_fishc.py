45bea312416837c9ab4f905886c44d5abe97dc52
jerome <jerome.lzh@gmail.com>
2019-09-14 18:29:31 +0800

    Clean the file list

--- 01_004_exercise_02_fishc.py
+++
@@ -1,6 +0,0 @@
-temp_int = input('Please input a positive integer number:')
-print (isinstance(temp_int, int))
-loop_num = int(temp_int)
-while loop_num:
-    print(' ' * (loop_num - 1) + '*' * loop_num)
-    loop_num = loop_num - 1
--- 01_007_008_exercise_01_fishc.py
+++
@@ -1,3 +0,0 @@
-x, y, z = 6, 5, 4
-small = x if (x < y and x < z) else (y if y < z else z)
-print('Smallest Number:', small)
