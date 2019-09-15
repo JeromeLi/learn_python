8b9f534838c1ed6f1ae8929367e0460f27cbeac5
jerome <jerome.lzh@gmail.com>
2019-09-15 21:34:38 +0800

    Remove some comments

--- web_crawler.py
+++ web_crawler.py
@@ -26,21 +26,7 @@ for x in range(page_count):
     req = urllib.request.Request(base_url)
     req.add_header = ('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0')
     html = urllib.request.urlopen(req, context=ssl._create_unverified_context()).read()
-    # print(type(html))
-    #
-    # print('write html context')
-    # fhandle = open('./origin.html', 'wb')
-    # fhandle.write(html)
-    # fhandle.close()
-    # break
-    # html_url = copy.deepcopy(html)
-    # print('write deepcopy context')
-    # fhandle = open('./deepcopy.html', 'wb')
-    # fhandle.write(html_url)
-    # fhandle.close()
     path_0 = re.findall(b'<a id="pb_next" href="(.+?)">', html)
-    # print(type(path_0))
-    # print(path_0)
     path_0_str = b''.join(path_0)
     path_0_str = path_0_str.decode()
     if path_0_str.find('/') != -1:
@@ -56,23 +42,10 @@ for x in range(page_count):
         next_page = 'https://m.piaotianzw.com/book_39768/' + path_0_str
         # print(next_page)
         base_url = next_page
-
-    # print('write deepcopy context after path process')
-    # fhandle = open('./deepcopy_path.html', 'wb')
-    # fhandle.write(html_url)
-    # fhandle.close()
-
     soup = BeautifulSoup(html, "html.parser")
     # kill all script and style elements
     for script in soup(["script", "style"]):
         script.extract()  # rip it out
-    # html_temp = html_url.read()
-    # print(type(html))
-    # print('write html context after soup process')
-    # fhandle = open('./origin_soup.html', 'wb')
-    # fhandle.write(html)
-    # fhandle.close()
-
     # get text
     text = soup.get_text()
     # break into lines and remove leading and trailing space on each
@@ -89,25 +62,7 @@ for x in range(page_count):
     file_handle = open(filename_str, 'wb')
     file_handle.write(file_context)
     file_handle.close()
-    # text = text.replace('关灯\n', '')
-    # text = text.replace('护眼\n', '')
-    # text = text.replace('字体：\n', '')
-    # text = text.replace('大\n', '')
-    # text = text.replace('中\n', '')
-    # text = text.replace('小\n', '')
-    # text = text.replace('首页\n', '')
-    # text = text.replace('书架\n', '')
-    # text = text.replace('加入书签\n', '')
-    # text = text.replace('返回目录\n', '')
-    # text = text.replace('上一章\n', '')
-    # text = text.replace('下一章\n', '')
-    # text = text.replace('目录\n', '')
-    # text = text.replace('上一页\n', '')
-    # text = text.replace('下一页\n', '')
-    # text = text.replace('电脑版\n', '')
-    # text = text.replace('我的书架', ''), '\n-->>本章未完，点击下一页继续阅读\n': '', '-->>本章未完，点击下一页继续阅读\n': '',
-    #                      '\n>本章未完，点击下一页继续阅读\n': '', '>本章未完，点击下一页继续阅读\n': ''
-    # print(type(text))
+
     if text.find('继续阅读') != -1:
         text = text_replace(text, replace_text_dict_nextpage)
         text = re.sub(r'\b第.+?网\n\b', '', text, re.MULTILINE)
@@ -116,12 +71,4 @@ for x in range(page_count):
     else:
         text = re.sub(r'\b第.+?网\n\b', '', text, re.MULTILINE)
         text = re.sub(r'\b第.+?\n\b', '', text, re.MULTILINE)
-        # text = text.replace('\n-->>本章未完，点击下一页继续阅读\n', '')
-        # text = text.replace('-->>本章未完，点击下一页继续阅读\n', '')
-        # text = text.replace('\n>本章未完，点击下一页继续阅读\n', '')
-        # text = text.replace('>本章未完，点击下一页继续阅读\n', '')
-
-        # final_text = re.sub(r'\b第.+?\n\b', '', final_text, re.MULTILINE)
-        # final_text = re.sub(r'^\n', '', final_text)
-        # print('continue')
         print(text)
