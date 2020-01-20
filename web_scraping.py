55179895807b478bdae3a7005d1d562ff4c5f04c
Jerome <jerome@MacBookPro.local>
2020-01-20 15:20:04 +0800

    add url input fuction; optimized base_url settings.

--- web_scraping.py
+++ web_scraping.py
@@ -1,8 +1,10 @@
 import re
 # from termcolor import colored
+# import backoff
 from os import path, system, name, kill, getpid
 from time import sleep
 import requests
+# from requests.exceptions import HTTPError
 from lxml.html import document_fromstring
 
 
@@ -47,11 +49,16 @@ def nomore_page_close(y):
 replace_symbol_dict = {'-->>': ''}
 ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
 headers = {'user-agent': ua}
-base_url = 'https://m.piaotianzw.com/book_39768/12675326.html'
+# base_url = 'https://m.piaotianzw.com/book_39768/12675326.html'
+# base_url = 'https://m.xbiquge.cc/book_5019/3181318.html'
+# base_url = 'https://m.xbiquge.cc/book_50449/33189706.html'
+# base_url = 'https://m.xbiquge.cc/book_23793/15039526.html'
+base_url = input('Input a url:')
 x = 0
 # Determine if the file name exists
 for n in range(5):
     clear()
+    global file_name
     file_name = input('Input a filename:')
     if not path.exists(file_name):
         break
@@ -61,11 +68,30 @@ for n in range(5):
             print('Program is terminating.....')
             sleep(2)
             kill(getpid(), 9)
-        print('The file name is existed! Please another file name.')
+        print('The file name is existed! Please input another file name.')
         sleep(2)
 
 while True:
-    r = requests.get(base_url, headers=headers)
+    try:
+        r = requests.get(base_url, headers=headers, timeout=10)
+    except (requests.ConnectionError,
+            requests.RequestException,
+            requests.HTTPError,
+            requests.Timeout,
+            requests.TooManyRedirects):
+        print('Connection Error! ReConnecting......')
+        continue
+    # r = requests_retry_session().get(base_url, headers=headers)
+    # r = requests_retry_session().get('https://m.xbiquge.cc/book_5019/3181318.html')
+    # print(r.status_code)
+    # while r.status_code != 200:
+    #     print('Time out! Retry......')
+    #     sleep(3)
+    #     r = requests.get(base_url, headers=headers)
+    #     if r.status_code == 200:
+    #         break
+        # print(r.status_code)
+    # break
     r.encoding = r.apparent_encoding
     doc = document_fromstring(r.text)
     # print(doc.xpath("//body[@id='nr_body']//h1")[0].text_content(),base_url)
@@ -83,6 +109,7 @@ while True:
             # print(next_click_text)
             content = text_replace(content, {next_click_text: ''})
             content = text_replace(content, replace_symbol_dict)
+            # print('Write Next Chapter!')
             add_title_fw()
             content = content[:-1]
             file_context = content.encode()
@@ -107,12 +134,17 @@ while True:
             write_to_file.write(file_context_return)
             # print(content)
             # print()
-        # print(title+' : '+base_url)
+        print(title+' : '+base_url)
+        # sleep(2)
         # print(text,base_url)
         # print(doc.xpath("//td[@class='next']//a")[1].get('href'))
         if '/' in doc.xpath("//td[@class='next']//a")[1].get('href'):
-            base_url = 'https://m.piaotianzw.com' + doc.xpath("//td[@class='next']//a")[1].get('href')
+            url_temp = base_url.split('/')[0] + '//' + base_url.split('/')[2]
+            # base_url = 'https://m.xbiquge.cc' + doc.xpath("//td[@class='next']//a")[1].get('href')
+            base_url = url_temp + doc.xpath("//td[@class='next']//a")[1].get('href')
             nomore_page_close(base_url)
         else:
-            base_url = 'https://m.piaotianzw.com/book_39768/' + doc.xpath("//td[@class='next']//a")[1].get('href')
+            url_temp = base_url.split('/')[0]+'//'+base_url.split('/')[2]+'/'+base_url.split('/')[3]+'/'
+            # base_url = 'https://m.xbiquge.cc/book_23793/' + doc.xpath("//td[@class='next']//a")[1].get('href')
+            base_url = url_temp + doc.xpath("//td[@class='next']//a")[1].get('href')
             nomore_page_close(base_url)
